410d7c2bc03a2259f4f3f287a1b4ab85ba2e66df
pedrocactus <pierrecastex@yahoo.fr>
2013-08-01 16:34:34 +0200

    Initial Project Commit

---
+++ PCSpinner/PCSpinner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PCSpinner
+//
+//  Created by avisto on 01/08/13.
+//  Copyright (c) 2013 com.pedrocactus.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
